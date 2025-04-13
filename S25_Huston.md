2266d3cd05b0b7442f0e361fd83d5b0e57d66955
Marlena Huston <mar2211312@maricopa.edu>
2025-04-13 00:15:31 -0700

    * No changes

---
+++ S25_Huston.md
@@ -0,0 +1,7 @@
+# Song Quote
+
+
+> Little black submarines, operator please.
+> Put me back on the line.
+>
+>The Black Keys, Little Black Submarines
---
+++ S25_Huston.py
@@ -0,0 +1,3 @@
+print("Little black submarines, operator please.")
+print("Put me back on the line.")
+print("The Black Keys, Little Black Submarines")
